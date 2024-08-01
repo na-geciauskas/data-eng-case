b289a920d90cbb0a5b6f971dd37c83343c940622
Nara Geciauskas Ramos Castillo <na.geciauskas@gmail.com>
2024-08-01 12:12:30 -0300

    Update aggregated_breweries_view.sql
    Changed name of 'location' to 'country'

--- aggregated_breweries_view.sql
+++ aggregated_breweries_view.sql
@@ -2,84 +2,84 @@
 CREATE OR REPLACE VIEW breweries_july_dataset.aggregated_breweries AS
 SELECT
   brewery_type,
-  location,
+  country,
   COUNT(*) AS quantity
 FROM (
   SELECT
     brewery_type,
-    'austria' AS location
+    'austria' AS country
   FROM
     `abinbev-case.breweries_july_dataset.austria_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'england' AS location
+    'england' AS country
   FROM
     `abinbev-case.breweries_july_dataset.england_brewweries`
   UNION ALL
   SELECT
     brewery_type,
-    'france' AS location
+    'france' AS country
   FROM
     `abinbev-case.breweries_july_dataset.france_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'ireland' AS location
+    'ireland' AS country
   FROM
     `abinbev-case.breweries_july_dataset.ireland_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'isle_of_man' AS location
+    'isle_of_man' AS country
   FROM
     `abinbev-case.breweries_july_dataset.isle_of_man_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'poland' AS location
+    'poland' AS country
   FROM
     `abinbev-case.breweries_july_dataset.poland_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'portugal' AS location
+    'portugal' AS country
   FROM
     `abinbev-case.breweries_july_dataset.portugal_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'scotland' AS location
+    'scotland' AS country
   FROM
     `abinbev-case.breweries_july_dataset.scotland_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'singapore' AS location
+    'singapore' AS country
   FROM
     `abinbev-case.breweries_july_dataset.singapore_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'south_korea' AS location
+    'south_korea' AS country
   FROM
     `abinbev-case.breweries_july_dataset.south_korea_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'sweden' AS location
+    'sweden' AS country
   FROM
     `abinbev-case.breweries_july_dataset.sweden_breweries`
   UNION ALL
   SELECT
     brewery_type,
-    'usa' AS location
+    'usa' AS country
   FROM
     `abinbev-case.breweries_july_dataset.usa_total_breweries`
 )
 GROUP BY
   brewery_type,
-  location
+  country
 ORDER BY
-  location,
+  country,
   brewery_type;
