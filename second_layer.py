a7a47daba9e2baf00b231178b97aeef4e9008a05
Nara Geciauskas Ramos Castillo <na.geciauskas@gmail.com>
2024-08-01 13:00:31 -0300

    Update second_layer.py

--- second_layer.py
+++ second_layer.py
@@ -1,28 +1,17 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+import pandas as pd
 
+# Read JSON file
+df = pd.read_json('/Users/naracastillo/Desktop/brews/all_breweries_data_july.json')
 
-# Initialize SparkSession
-spark = SparkSession.builder \
-    .appName("BreweriesDataTransformation") \
-    .getOrCreate()
+# Function - Substitute empty spaces for underscores
+def replace_spaces_with_underscores(column):
+    return column.str.replace(' ', '_')
 
-# Load raw JSON data into DataFrame
-raw_brew_data_df = spark.read.json("all_breweries_data.json", multiLine = True)
+# Apply transformation function to all string columns
+df = df.apply(lambda col: replace_spaces_with_underscores(col) if col.dtype == 'object' else col)
 
-# Perform transformations 
-breweries_df = raw_brew_data_df.select(
-    "id", "name", "brewery_type", 
-    "address_1", "address_2", "address_3", 
-    "city", "state_province", "postal_code", 
-    "country", "longitude", "latitude", 
-    "phone", "website_url", "state", "street"
-)
-
-# Partition data by brewery location
-breweries_df.write.partitionBy(
-    "country", "id"
-).parquet("breweries_location_parquet", mode="overwrite")
-
-# Stop SparkSession
-spark.stop()
+# Save DataFrame as partitioned Parquet 
+df.to_parquet('/Users/naracastillo/Desktop/breweries_by_country_only.parquet', 
+              engine='pyarrow', 
+              partition_cols=['country'], 
+              index=False)
